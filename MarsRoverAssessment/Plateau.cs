b8b58d9a26be8cd7d3ab003319585fecdda1510f
ThabangSirrve <tslekwadi@gmail.com>
2019-06-30 15:34:57 +0200

    Added Point class for Rover position and Plateau class to be navigated by Rovers

---
+++ MarsRoverAssessment/Plateau.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System .Collections.Generic;
+using System.Text;
+
+namespace MarsRoverAssessment
+{
+    public class Plateau
+    {
+        public readonly Point StartPosition;
+
+        public readonly Point EndPosition;
+
+        public Plateau(string plateauCoordinates)
+        {
+            //Validation
+            string[] coordinates = plateauCoordinates.Split(' ');
+            int x = Convert.ToInt32(coordinates[0]);
+            int y = Convert.ToInt32(coordinates[1]);
+
+            StartPosition = new Point(0, 0);
+            EndPosition = new Point(x,y);
+        }
+    }
+}
