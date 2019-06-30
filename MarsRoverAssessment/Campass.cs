dc136176d97d6c09c76e3d58e1c68e9a1e6dde67
Thabang <tslekwadi@gmail.com>
2019-07-01 00:05:15 +0200

    Added Constant fields for the terrain

---
+++ MarsRoverAssessment/Campass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoverAssessment
+{
+    public class Campass
+    {
+        public const char LEFT = 'L';
+        public const char RIGHT = 'R';
+        public const char EAST = 'E';
+        public const char NORTH = 'N';
+        public const char WEST = 'W';
+        public const char SOUTH = 'S';
+        public const char MOVE = 'M';
+    }
+}
