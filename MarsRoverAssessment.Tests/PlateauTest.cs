d3596405246bcd40b489a90ef3b35c10f94fdbae
Thabang <tslekwadi@gmail.com>
2019-07-01 08:00:24 +0200

    Added Test cases for Rover and Plateau

---
+++ MarsRoverAssessment.Tests/PlateauTest.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace MarsRoverAssessment.Tests
+{
+    public class PlateauTest
+    {
+        [Fact]
+        public void CreatePlateau()
+        {
+            // Arrange
+            Plateau plateau = new Plateau("5 5");
+
+            // Act
+            var startPosition = plateau.StartPosition;
+            var endPosition = plateau.EndPosition;
+           
+            // Assert
+            Assert.NotNull(startPosition);
+            Assert.NotNull(endPosition);
+            Assert.Equal("5",plateau.EndPosition.Y.ToString());
+            Assert.Equal("5",plateau.EndPosition.X.ToString());
+            Assert.True((endPosition.X > 0 && endPosition.Y > 0));
+        }
+    }
+}
---
+++ MarsRoverAssessment.Tests/RoverTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MarsRoverAssessment.Tests
+{
+    public class RoverTest
+    {
+        [Fact]
+        public void PlaceRover()
+        {
+            // Arrange
+            Plateau plateau = new Plateau("5 5");
+            Rover rover = new Rover("1 2 N", plateau);
+
+            // Act
+            rover.Explore("LMLMLMLMM");
+
+            // Assert
+            Assert.NotNull(rover);
+            Assert.True((rover.Position.X < plateau.EndPosition.X && rover.Position.Y < plateau.EndPosition.Y));
+            Assert.Equal("1 3 N", rover.RoverDestination());
+        }
+    }
+}
